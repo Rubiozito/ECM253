433406b9c8ba64165b9d5dcf699a268c4981d0d8
Rubiozito <rafaelrubiocarnes@gmail.com>
2023-08-25 20:01:42 -0300

    Classes Tag, Token, Word

---
+++ Lexer/src/Tag.java
@@ -0,0 +1,25 @@
+public enum Tag {
+    // Palavras reservadas
+    IF, ELSE, WHILE, DO, INT, CHAR, VOID,
+    
+    // Identificador
+    IDENTIFIER,
+    
+    // Constantes
+    INTEGER_CONSTANT, HEX_CONSTANT, OCTAL_CONSTANT, REAL_CONSTANT,
+    
+    // Cadeias de caracteres e caracteres
+    STRING, CHAR_LITERAL,
+    
+    // Operadores
+    ARROW, INCREMENT, DECREMENT, AND, MULTIPLY, PLUS, MINUS, TILDE, NOT, COMMA,
+    
+    // Pontuadores
+    LEFT_BRACKET, RIGHT_BRACKET, LEFT_PAREN, RIGHT_PAREN, SEMICOLON,
+    
+    // Ignorar espaços em branco e comentários
+    WHITESPACE, COMMENT,
+    
+    // Erro
+    ERROR
+}
---
+++ Lexer/src/Token.java
@@ -0,0 +1,18 @@
+public class Token {
+    private Tag tag;
+    
+    public Token(Tag tag) {
+        this.tag = tag;
+    }
+    
+    public Tag getTag() {
+        return tag;
+    }
+
+    @Override
+    public String toString() {
+        return "Token [tag=" + tag + "]";
+    }
+    
+    
+}
